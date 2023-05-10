4e4727f2d52c07beb80ffdd100ceabcde6c5d45a
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2023-05-10 19:30:32 +0200

    pas fini

---
+++ jour2/job1.java
@@ -0,0 +1,12 @@
+package jour2;
+
+public class job1 {
+    public static void main(String[] args) {
+        int nb = 0;
+        while(nb < 100){
+            nb +=1;
+            System.out.println(nb);
+        }
+
+    }
+}
---
+++ jour2/job2.java
@@ -0,0 +1,12 @@
+package jour2;
+
+public class job2 {
+    public static void main(String[] args) {
+        int nb = 101;
+        while(nb > 1){
+            nb -=1;
+            System.out.println(nb);
+        }
+
+    }
+}
